074593d7a86ce16306eeb4d12481aa331b5b703d
Vinhvip0089 <phanthanhtam618678@gmail.com>
2024-12-19 20:04:43 +0700

    submit

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -5,7 +5,9 @@ import RegisterPage from './pages/User/RegisterPage';
 import Profile from './pages/User/Profile';
 import ForgotPassword from './pages/User/ForgotPassword';
 import Home from './pages/User/Home';
-
+import RtlLayout from "layouts/rtl";
+import AdminLayout from "layouts/admin";
+import AuthLayout from "layouts/auth";
 const App = () => {
   return (
     <Router>
@@ -15,6 +17,10 @@ const App = () => {
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/" element={<Home />} />
+        <Route path="auth/*" element={<AuthLayout />} />
+        <Route path="admin/*" element={<AdminLayout />} />
+        <Route path="rtl/*" element={<RtlLayout />} />
+        <Route path="/admin" />
       </Routes>
     </Router>
   );
